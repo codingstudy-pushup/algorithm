2ff930684d13ca8d0b62373954eaecc1d8068c1b
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2021-10-24 22:58:31 +0900

    Create A007_LruCache_Origin.java

---
+++ A007_LruCache_Origin.java
@@ -0,0 +1,101 @@
+
+
+import java.util.*;
+
+class Node {
+	int key;
+	int value;
+	Node pre;
+	Node next;
+
+	public Node(int key, int value) {
+		this.key = key;
+		this.value = value;
+	}
+}
+
+public class A007_LruCache_Origin {
+
+	public static void main(String[] args) throws Exception {
+		A007_LruCache_Origin cache = new A007_LruCache_Origin(2);
+		cache.put(1, 1);
+		cache.put(2, 2);
+		System.out.println(cache.get(1));
+		cache.put(3, 3);
+		System.out.println(cache.get(2));
+		cache.put(4, 4);
+		System.out.println(cache.get(1));
+		System.out.println(cache.get(3));
+		System.out.println(cache.get(4));
+	}
+
+	int capacity;
+	HashMap<Integer, Node> map = new HashMap<Integer, Node>();
+	Node head = null;
+	Node end = null;
+
+	public A007_LruCache_Origin(int capacity) {
+		this.capacity = capacity;
+	}
+
+	public int get(int key) {
+		if (map.containsKey(key)) {
+			Node n = map.get(key);
+			remove(n);
+			setHead(n);
+			return n.value;
+		}
+
+		return -1;
+	}
+
+	public void remove(Node n) {
+		if (n.pre != null) {
+			n.pre.next = n.next;
+		} else {
+			head = n.next;
+		}
+
+		if (n.next != null) {
+			n.next.pre = n.pre;
+		} else {
+			end = n.pre;
+		}
+
+	}
+
+	public void setHead(Node n) {
+		n.next = head;
+		n.pre = null;
+
+		if (head != null)
+			head.pre = n;
+
+		head = n;
+
+		if (end == null)
+			end = head;
+	}
+
+	public void put(int key, int value) {
+		if (map.containsKey(key)) {
+			Node old = map.get(key);
+			old.value = value;
+			remove(old);
+			setHead(old);
+		} else {
+			Node created = new Node(key, value);
+			if (map.size() >= capacity) {
+				map.remove(end.key);
+				remove(end);
+				setHead(created);
+
+			} else {
+				setHead(created);
+			}
+
+			map.put(key, created);
+		}
+	}
+
+}
